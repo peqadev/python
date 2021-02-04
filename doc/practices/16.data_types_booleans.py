1009eb0c5884c99c992b1577e495fa36205eeb4d
Erick Agrazal <erick.agrazal@gmail.com>
2021-02-03 21:51:05 -0500

    Added booleans and operators.

---
+++ doc/practices/16.data_types_booleans.py
@@ -0,0 +1,18 @@
+# Booleans
+# True, False
+
+print(10 > 9)  # True
+print(10 == 9)  # False
+print(10 < 9)  # False
+
+
+a = 100
+b = 100
+if a == b:
+    print('a y b son iguales')
+
+# Casting a bool
+print(bool("Hello"))  # True
+print(bool(""))  # False
+print(bool(1))  # True
+print(bool(0))  # False
---
+++ doc/practices/17.operators.py
@@ -0,0 +1,51 @@
+# Operadores python
+# +         Suma                 x + y
+# -         Sustracción          x - y
+# *         Multiplicación       x * y
+# /         División             x / y
+# %         Módulo               x % y
+# **        Exponenciación       x ** y
+
+# Operadores de asignación
+# =         x = 5       x = 5       
+# +=        x += 3      x = x + 3       
+# -=        x -= 3      x = x - 3       
+# *=        x *= 3      x = x * 3       
+# /=        x /= 3      x = x / 3       
+# %=        x %= 3      x = x % 3       
+# //=       x //= 3     x = x // 3      
+# **=       x **= 3     x = x ** 3      
+# &=        x &= 3      x = x & 3       
+# |=        x |= 3      x = x | 3       
+# ^=        x ^= 3      x = x ^ 3       
+# >>=       x >>= 3     x = x >> 3      
+# <<=       x <<= 3     x = x << 3
+
+# Operadores de comparación
+# ==        Igual               x == y
+# !=        No es igual a       x != y
+# >         Mayor que           x > y
+# <         Menor que           x < y
+# >=        Mayor o igual que   x >= y
+# <=        Menor o igual que   x <= y
+
+# Operadores lógicos
+# and       Retorna "True" si ambos elementos son verdaderos
+# or        Retorna "True" si alguno de los elementos son verdaderos
+# not       Retorna "False" si es verdadero y "True" si es falso
+
+# Operadores de identidad
+# is        Retorna "True" si ambas variable son el mismo objeto.
+# is no     Retorna "True" si ambas variable NO son el mismo objeto.
+
+# Operadores de pertenencia
+# in        Retorna "True" si el valor x pertenece a la secuencia y. "x in y".
+# not in    Retorna "True" si el valor x NO pertenece a la secuencia y. "x not in y".
+
+# Operadores "Bitwise"
+# &         AND
+# |         OR
+# ^         XOR
+# ~         NOT
+# <<        Zero fill left shift
+# >>        Signed right shift
